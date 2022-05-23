4afa52a23e3ca16f9d0c1aa101384aa31897db2f
AARON NYAGA <101546697+RonZoro11@users.noreply.github.com>
2022-05-23 15:00:27 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,22 @@
-include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#include <stdio.h>
 
-	return (0);
+ int main(void)
+{
+	int n = 0;
+	int i;
+
+
+	for (i=0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+		n += i;
+		
+
+		if (i % 5 == 0)
+		n += i;
+	}
+
+	printf("%i\n", n);
+
+	return 0;
 }
